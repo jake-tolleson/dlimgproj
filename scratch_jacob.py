dd1cb2c770bcbb38cbf6b5e3bc46b0d83e7eec5b
Jacob Heinrich <jheinri1@vols.utk.edu>
2022-05-02 13:37:32 -0400

    Update scratch_jacob.py

--- scratch_jacob.py
+++ scratch_jacob.py
@@ -15,6 +15,8 @@ def crop_digit(img, json_file, digit):
     pic = img[json_file['digitStruct'][digit]['bbox']['y1']:json_file['digitStruct'][digit]['bbox']['y2'],
                 json_file['digitStruct'][digit]['bbox']['x1']:json_file['digitStruct'][digit]['bbox']['x2']]
     return(pic)
+crop_digit(train_img_path, train_json_path, 0)
+crop_digit(test_img_path, test_json_path, 0)
 
 # Train CNN based on all individual number images
 def train_cnn(train_img_path, train_json_path, test_img_path, test_json_path):
@@ -66,7 +68,7 @@ def find_max_dim(data):
         if data['digitStruct'][i]['bbox']['width'] > max_dim:
             max_dim = data['digitStruct'][i]['bbox']['width']
     return(max_dim)
-    
+
 # Pad all other images to match this dimension
 def pad_images(data, max_dim):
     for i in range(len(data)):
@@ -85,13 +87,16 @@ def scale_data(data):
         data['digitStruct'][i]['bbox']['y2'] = data['digitStruct'][i]['bbox']['y2'] / 255
     return(data)
 
-# Translate Data Labels into one-hot encodings
-def convert_digit_to_array(digit):
-    digit_array = np.zeros(10)
-    digit_array[digit] = 1
-    return(digit_array)
-# 
+##############################
+#######Preprocess data########
+##############################
+max_dim = find_max_dim(train_data)
+train_data = pad_images(train_data, max_dim)
+train_data = scale_data(train_data)
 
+max_dim = find_max_dim(test_data)
+test_data = pad_images(test_data, max_dim)
+test_data = scale_data(test_data)
 
 # Convert labels to one-hot encodings
 def convert_digit_to_array(digit):
@@ -115,6 +120,8 @@ def convert_digit_to_array(digit):
         return(np.array([0,0,0,0,0,0,0,0,1,0]))
     if digit == 9.0:
         return(np.array([0,0,0,0,0,0,0,0,0,1]))
+
+convert_digit_to_array(test_data['digitStruct'][0]['label'])
 
 
 train_data['label'] = 0
