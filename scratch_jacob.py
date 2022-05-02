a0206c67356a107a8ae3dbbd9f6b9405328e108b
Jacob Heinrich <jheinri1@vols.utk.edu>
2022-05-02 13:55:10 -0400

    Update scratch_jacob.py

--- scratch_jacob.py
+++ scratch_jacob.py
@@ -20,55 +20,12 @@ crop_digit(train_img_path, train_json_path, 0)
 crop_digit(test_img_path, test_json_path, 0)
 
 # Train CNN based on all individual number images
-def train_cnn(train_img_path, train_json_path, test_img_path, test_json_path):
-    cnn = tf.keras.models.Sequential()
-    cnn.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-    cnn.add(tf.keras.layers.MaxPooling2D((2, 2)))
-    cnn.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-    cnn.add(tf.keras.layers.MaxPooling2D((2, 2)))
-    cnn.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
-    cnn.add(tf.keras.layers.MaxPooling2D((2, 2)))
-    cnn.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
-    cnn.add(tf.keras.layers.MaxPooling2D((2, 2)))
-    cnn.add(tf.keras.layers.Flatten())
-    cnn.add(tf.keras.layers.Dense(512, activation='relu'))
-    cnn.add(tf.keras.layers.Dense(10, activation='softmax'))
-    cnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    train_img = []
-    train_label = []
-    test_img = []
-    test_label = []
-    for i in range(10):
-        train_json = pd.read_json(train_json_path)
-        train_img.append(crop_digit(train_img_path, train_json, i))
-        train_label.append(convert_digit_to_array(i))
-    for i in range(10):
-        test_json = pd.read_json(test_json_path)
-        test_img.append(crop_digit(test_img_path, test_json, i))
-        test_label.append(convert_digit_to_array(i))
-    train_img = np.array(train_img)
-    train_label = np.array(train_label)
-    test_img = np.array(test_img)
-    test_label = np.array(test_label)
-    cnn.fit(train_img, train_label, epochs=5, batch_size=32)
-    test_loss, test_acc = cnn.evaluate(test_img, test_label)
-    print('Test accuracy:', test_acc)
-    
 
-train_data = pd.read_json(train_json_path)
-test_data = pd.read_json(test_json_path)
 
 ##maybe try paddings vs rescaling
 ## DATA PREPROCESSING
 # Find dimensions of largest image in data
-def find_max_dim(data):
-    max_dim = 0
-    for i in range(len(data)):
-        if data['digitStruct'][i]['bbox']['height'] > max_dim:
-            max_dim = data['digitStruct'][i]['bbox']['height']
-        if data['digitStruct'][i]['bbox']['width'] > max_dim:
-            max_dim = data['digitStruct'][i]['bbox']['width']
-    return(max_dim)
+
 
 # Pad all other images to match this dimension
 def pad_images(data, max_dim):
@@ -88,16 +45,6 @@ def scale_data(data):
         data['digitStruct'][i]['bbox']['y2'] = data['digitStruct'][i]['bbox']['y2'] / 255
     return(data)
 
-##############################
-#######Preprocess data########
-##############################
-max_dim = find_max_dim(train_data)
-train_data = pad_images(train_data, max_dim)
-train_data = scale_data(train_data)
-
-max_dim = find_max_dim(test_data)
-test_data = pad_images(test_data, max_dim)
-test_data = scale_data(test_data)
 
 # Convert labels to one-hot encodings
 def convert_digit_to_array(digit):
