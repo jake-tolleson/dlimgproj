ef1a72258d4da7678a44e9ac1766cc16114d41de
Andrew Cate <73675851+andrewtcate@users.noreply.github.com>
2022-05-05 15:17:16 -0400

    Updated

--- Andrew_DL_3.py
+++ Andrew_DL_3.py
@@ -4,6 +4,7 @@ import numpy as np
 import tensorflow as tf
 import os
 import cv2
+import matplotlib.pyplot as plt
 
 # Getting image directories
 train_img_path = 'C:/Users/Andrew/Desktop/Spring 2022/BZAN 554/Group Assignment/3/SVHN/train/'
@@ -51,6 +52,8 @@ def convert_digit_to_array(digit):
         return(np.array([0,0,0,0,0,0,0,0,1,0]))
     if digit == 9.0:
         return(np.array([0,0,0,0,0,0,0,0,0,1]))
+    if digit == 10.0:
+        return(np.array([1,0,0,0,0,0,0,0,0,0]))
 
 
 
@@ -62,9 +65,13 @@ for index, label in enumerate(train_data['boxes']):
     digit_list = []
     for digit in label:
         digit_list.append(convert_digit_to_array(digit['label']))
+    while len(digit_list) < 6:
+        digit_list.append(np.array([0,0,0,0,0,0,0,0,0,0]))
+    digit_list.append(convert_digit_to_array(len(label)))
     train_data['label'].loc[index] = digit_list
 
-
+#from itertools import chain
+#np.array(list(chain.from_iterable(train_data['label'][0])))
 
 # Creating digit labels for test data
 test_data['label'] = 0
@@ -74,6 +81,9 @@ for index, label in enumerate(test_data['boxes']):
     digit_list = []
     for digit in label:
         digit_list.append(convert_digit_to_array(digit['label']))
+    while len(digit_list) < 6:
+        digit_list.append(np.array([0,0,0,0,0,0,0,0,0,0]))
+    digit_list.append(convert_digit_to_array(len(label)))
     test_data['label'].loc[index] = digit_list
 
 
@@ -101,17 +111,17 @@ train_data.boxes[0] # we need min(top) and min(left) from all BBs. Then we do to
 # Finding BB for train Data
 train_data['BB'] = 0
 train_data = train_data.astype('object')
-
+train_data.boxes[0]
 for index, label in enumerate(train_data.boxes):
-    topLeftX = 0
-    topLeftY = 0
+    topLeftX = np.Inf
+    topLeftY = np.Inf
     bottomRightX = 0
     bottomRightY = 0
     
     for digit in label:
-        if digit['top'] > topLeftY:
+        if digit['top'] < topLeftY:
             topLeftY = digit['top']
-        if digit['left'] > topLeftX:
+        if digit['left'] < topLeftX:
             topLeftX = digit['left']
         if digit['top'] + digit['height'] > bottomRightY:
             bottomRightY = digit['top'] + digit['height']
@@ -126,15 +136,15 @@ test_data['BB'] = 0
 test_data = test_data.astype('object')
 
 for index, label in enumerate(test_data.boxes):
-    topLeftX = 0
-    topLeftY = 0
+    topLeftX = np.Inf
+    topLeftY = np.Inf
     bottomRightX = 0
     bottomRightY = 0
     
     for digit in label:
-        if digit['top'] > topLeftY:
+        if digit['top'] < topLeftY:
             topLeftY = digit['top']
-        if digit['left'] > topLeftX:
+        if digit['left'] < topLeftX:
             topLeftX = digit['left']
         if digit['top'] + digit['height'] > bottomRightY:
             bottomRightY = digit['top'] + digit['height']
@@ -142,9 +152,6 @@ for index, label in enumerate(test_data.boxes):
             bottomRightX = digit['left'] + digit['width']
         
         test_data['BB'].loc[index] = [topLeftX,topLeftY,bottomRightX,bottomRightY]
-
-
-
 
 
 # We need to go through and find the largest image in the data and pad all others to that size...
@@ -161,8 +168,8 @@ for image in range(len(train_data)):
     if height > train_max_height:
         train_max_height = height
 
-print(train_max_height) # 403 - train
-print(train_max_width) # 207 -train
+print(train_max_height) # 423 - train
+print(train_max_width) # 616 -train
 
 test_max_height = 0
 test_max_width = 0
@@ -177,59 +184,150 @@ for image in range(len(test_data)):
     if height > test_max_height:
         test_max_height = height
 
-print(test_max_height) # 208 - test
-print(test_max_width) # 133 - test
+print(test_max_height) # 222 - test
+print(test_max_width) # 206 - test
 
 
 
-# So we need to crop to the bounding pad all images to 403 x 207
+
+
+
+
+
+
 train_cropped_path = 'C:/Users/Andrew/Desktop/Spring 2022/BZAN 554/Group Assignment/3/SVHN/train/cropped/'
 test_cropped_path = 'C:/Users/Andrew/Desktop/Spring 2022/BZAN 554/Group Assignment/3/SVHN/test/cropped/'
 
-for image in range(len(test_data)):
-    img_path = os.path.join(train_img_path, train_data['filename'].loc[image])
-    image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    cropped_image = image[train_data.BB[image][0]:train_data.BB[image][2],train_data.BB[image][1]:train_data.BB[image][3]]
-    
-    old_image_width = train_data.BB[image][2] - train_data.BB[image][0]
-    old_image_height = train_data.BB[image][3] - train_data.BB[image][1]
-    
+
+######### ONLY NEED TO RUN THIS ONCE
+
+# So we need to crop to the bounding pad all images to 423 x 616
+
+for idx in range(len(train_data)):
+    img_path = os.path.join(train_img_path, train_data['filename'].loc[idx])
+    image = cv2.imread(img_path)
+    cropped_image = image[int(train_data.BB[idx][1]):int(train_data.BB[idx][3]),int(train_data.BB[idx][0]):int(train_data.BB[idx][2])]
+    old_image_height, old_image_width, channels = cropped_image.shape
+
     # create new image of desired size and color (white) for padding
-    new_image_width = max([train_max_width,test_max_width])
-    new_image_height = max([train_max_height,test_max_height])
+    new_image_width = int(max([train_max_width,test_max_width]))
+    new_image_height = int(max([train_max_height,test_max_height]))
     color = (255,255,255)
-    result = np.full((new_image_height,new_image_width), color, dtype=np.uint8)
+    result = np.full((new_image_height,new_image_width,3), color, dtype=np.uint8)
 
     # compute center offset
-    x_center = (new_image_width - old_image_width) // 2
-    y_center = (new_image_height - old_image_height) // 2
+    x_center = int((new_image_width - old_image_width) // 2)
+    y_center = int((new_image_height - old_image_height) // 2)
 
     # copy img image into center of result image
     result[y_center:y_center+old_image_height, x_center:x_center+old_image_width] = cropped_image
 
     # save result
-    cv2.imwrite(os.path.join(train_cropped_path , train_data['filename'].loc[image]), result)
+    cv2.imwrite(os.path.join(train_cropped_path , train_data['filename'].loc[idx]), result)
 
 
-img_path = os.path.join(train_img_path, train_data['filename'].loc[0])
-image = cv2.imread(img_path)
-cropped_image = image[int(train_data.BB[0][0]):int(train_data.BB[0][2]),int(train_data.BB[0][1]):int(train_data.BB[0][3])]
+for idx in range(len(test_data)):
+    img_path = os.path.join(test_img_path, test_data['filename'].loc[idx])
+    image = cv2.imread(img_path)
+    cropped_image = image[int(test_data.BB[idx][1]):int(test_data.BB[idx][3]),int(test_data.BB[idx][0]):int(test_data.BB[idx][2])]
+    old_image_height, old_image_width, channels = cropped_image.shape
 
-old_image_width = int(train_data.BB[0][2] - train_data.BB[0][0])
-old_image_height = int(train_data.BB[0][3] - train_data.BB[0][1])
+    # create new image of desired size and color (white) for padding
+    new_image_width = int(max([train_max_width,test_max_width]))
+    new_image_height = int(max([train_max_height,test_max_height]))
+    color = (255,255,255)
+    result = np.full((new_image_height,new_image_width,3), color, dtype=np.uint8)
 
-# create new image of desired size and color (white) for padding
-new_image_width = int(max([train_max_width,test_max_width]))
-new_image_height = int(max([train_max_height,test_max_height]))
-color = (255,255,255)
-result = np.full((new_image_width,new_image_height,3), color, dtype=np.uint8)
+    # compute center offset
+    x_center = int((new_image_width - old_image_width) // 2)
+    y_center = int((new_image_height - old_image_height) // 2)
 
-# compute center offset
-x_center = int((new_image_width - old_image_width) // 2)
-y_center = int((new_image_height - old_image_height) // 2)
+    # copy img image into center of result image
+    result[y_center:y_center+old_image_height, x_center:x_center+old_image_width] = cropped_image
 
-# copy img image into center of result image
-result[y_center:y_center+old_image_height, x_center:x_center+old_image_width] = cropped_image
+    # save result
+    cv2.imwrite(os.path.join(test_cropped_path , test_data['filename'].loc[idx]), result)
 
-# save result
-cv2.imwrite(os.path.join(train_cropped_path , train_data['filename'].loc[image]), result)
+
+# Now we have all the data in the format that we want it to be in.
+
+
+
+###### END OF ONLY NEED TO RUN THIS ONCE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+train_cropped_path = 'C:/Users/Andrew/Desktop/Spring 2022/BZAN 554/Group Assignment/3/SVHN/train/cropped/'
+test_cropped_path = 'C:/Users/Andrew/Desktop/Spring 2022/BZAN 554/Group Assignment/3/SVHN/test/cropped/'
+
+
+# Need to make train and test values
+train = train_data.drop(columns=['boxes','BB'])
+
+# creating a column to hold images
+train['image'] = 0
+train = train.astype('object')
+
+for idx,img in enumerate(train['filename']):
+    img_path = os.path.join(train_cropped_path, img)
+    train['image'].loc[idx] = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE) / 255.0
+    #train['label'].loc[idx] = tf.convert_to_tensor(train['label'][idx])
+
+test = test_data.drop(columns=['boxes','BB'])
+test['image'] = 0
+test = test.astype('object')
+
+for idx,img in enumerate(test['filename']):
+    img_path = os.path.join(test_cropped_path, img)
+    test['image'].loc[idx] = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE) / 255.0
+    test['label'].loc[idx] = tf.convert_to_tensor(test['label'][idx])
+
+
+inputs = tf.keras.layers.Input(shape=(int(max([train_max_height,test_max_height])),int(max([train_max_width,test_max_width])),1), name='input') 
+x = tf.keras.layers.Conv2D(filters=64,kernel_size = 7, strides = 1, padding = "same", activation = "relu")(inputs)
+x = tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2, padding = "valid")(x)
+x = tf.keras.layers.Conv2D(filters=128,kernel_size = 3, strides = 1, padding = "same", activation = "relu")(x)
+x = tf.keras.layers.Conv2D(filters=128,kernel_size = 3, strides = 1, padding = "same", activation = "relu")(x)
+x = tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2, padding = "valid")(x)
+x = tf.keras.layers.Conv2D(filters=256,kernel_size = 3, strides = 1, padding = "same", activation = "relu")(x)
+x = tf.keras.layers.Conv2D(filters=256,kernel_size = 3, strides = 1, padding = "same", activation = "relu")(x)
+x = tf.keras.layers.MaxPooling2D(pool_size = 2, strides = 2, padding = "valid")(x)
+x = tf.keras.layers.Flatten()(x)
+x = tf.keras.layers.Dense(128, activation = 'relu')(x)
+x = tf.keras.layers.Dense(64, activation = 'relu')(x)
+yhat_1 = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+yhat_2 = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+yhat_3 = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+yhat_4 = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+yhat_5 = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+yhat_6 = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+yhat_length = tf.keras.layers.Dense(10, activation = 'softmax')(x)
+
+model = tf.keras.Model(inputs = inputs, outputs = [yhat_1, yhat_2, yhat_3, yhat_4, yhat_5, yhat_6, yhat_length])
+model.summary()
+#Compile model
+model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001))
+
+#Fit model
+model.fit(x=train['image'][0],y=train['label'][0], batch_size=1, epochs=1) 
+train['image'][3].shape
+train['label'][0]
+
+#Compute multiclass accuray
+yhat = model.predict(x=X_test)
+yhat_sparse = [int(np.where(yhat_sub ==np.max(yhat_sub))[0]) for yhat_sub in yhat]
+y_test
+sum(yhat_sparse == y_test) / len(y_test)
+
+
